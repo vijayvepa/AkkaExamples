038dcd5849a64c9985b9c618edbacce6a60ce1e2
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2022-09-18 17:24:50 -0400

    events route

--- AkkaInAction/UpAndRunning/app/src/main/scala/upandrunning/api/BoxOfficeRoutes.scala
+++ AkkaInAction/UpAndRunning/app/src/main/scala/upandrunning/api/BoxOfficeRoutes.scala
@@ -1,18 +1,57 @@
 package upandrunning.api
-import akka.http.scaladsl.model.StatusCodes._
+
+import akka.actor.*
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.*
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.*
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.server.Directives.*
+import akka.pattern.ask
+import akka.util.Timeout
+import upandrunning.actors.BoxOffice
+
 trait BoxOfficeRoutes extends BoxOfficeApi with BoxOfficeMarshalling {
 
-  def eventsRoute =
+  import StatusCodes.*
+
+  def eventRoute: Route =
+    pathPrefix("events" / Segment) { event =>
+      pathEndOrSingleSlash {
+        post {
+          // POST /events/:event
+          entity(as[EventDescription]) { ed =>
+            onSuccess(createEvent(event, ed.tickets)) {
+              case BoxOffice.EventCreated(event) => complete(Created, event)
+              case BoxOffice.EventExists =>
+                val err = Error(s"$event event exists already.")
+                complete(BadRequest, err)
+            }
+          }
+        } ~
+          get {
+            // GET /events/:event
+            onSuccess(getEvent(event)) {
+              _.fold(complete(NotFound))(e => complete(OK, e))
+            }
+          } ~
+          delete {
+            // DELETE /events/:event
+            onSuccess(cancelEvent(event)) {
+              _.fold(complete(NotFound))(e => complete(OK, e))
+            }
+          }
+      }
+    }
+
+  def eventsRoute: Route =
     pathPrefix("events") {
       pathEndOrSingleSlash {
-        get{
+        get {
           onSuccess(getEvents()) { events =>
             complete(OK, events)
           }
         }
       }
     }
+
+
 }
