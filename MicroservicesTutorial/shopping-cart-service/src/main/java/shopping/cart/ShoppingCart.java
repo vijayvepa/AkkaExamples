02d12bed1e6d6604391e8ce791b76c58999c69f4
Vijay <vijay.k.vepakomma@gmail.com>
2023-03-31 08:00:12 -0400

    handle checkout command

--- MicroservicesTutorial/shopping-cart-service/src/main/java/shopping/cart/ShoppingCart.java
+++ MicroservicesTutorial/shopping-cart-service/src/main/java/shopping/cart/ShoppingCart.java
@@ -1,5 +1,6 @@
 package shopping.cart;
 
+import akka.actor.typed.ActorRef;
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.SupervisorStrategy;
@@ -11,16 +12,21 @@ import akka.pattern.StatusReply;
 import akka.persistence.typed.PersistenceId;
 import akka.persistence.typed.javadsl.CommandHandlerWithReply;
 import akka.persistence.typed.javadsl.CommandHandlerWithReplyBuilder;
+import akka.persistence.typed.javadsl.CommandHandlerWithReplyBuilderByState;
 import akka.persistence.typed.javadsl.EventHandler;
 import akka.persistence.typed.javadsl.EventSourcedBehavior;
 import akka.persistence.typed.javadsl.EventSourcedBehaviorWithEnforcedReplies;
 import akka.persistence.typed.javadsl.ReplyEffect;
 import shopping.cart.command.AddItem;
+import shopping.cart.command.Checkout;
+import shopping.cart.event.CheckedOut;
 import shopping.cart.event.ItemAdded;
 import shopping.cart.event.ShoppingCartEvent;
 import shopping.cart.model.ShoppingCartState;
+import shopping.cart.model.Summary;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Optional;
 
@@ -29,18 +35,18 @@ public class ShoppingCart extends EventSourcedBehaviorWithEnforcedReplies<Shoppi
   static final EntityTypeKey<ShoppingCartCommand> ENTITY_TYPE_KEY =
       EntityTypeKey.create(ShoppingCartCommand.class, "ShoppingCart");
 
-  private final  String cartId;
+  private final String cartId;
 
-  public static void init(ActorSystem<?> system){
+  public static void init(ActorSystem<?> system) {
     final ClusterSharding clusterSharding = ClusterSharding.get(system);
-    clusterSharding.init(Entity.of(ENTITY_TYPE_KEY, entityContext->ShoppingCart.create(entityContext.getEntityId())));
+    clusterSharding.init(Entity.of(ENTITY_TYPE_KEY, entityContext -> ShoppingCart.create(entityContext.getEntityId())));
   }
 
   public static Behavior<ShoppingCartCommand> create(String cartId) {
-    return Behaviors.setup(ctx-> EventSourcedBehavior.start(new ShoppingCart(cartId), ctx));
+    return Behaviors.setup(ctx -> EventSourcedBehavior.start(new ShoppingCart(cartId), ctx));
   }
 
-  private ShoppingCart(String cartId){
+  private ShoppingCart(String cartId) {
     super(PersistenceId.of(ENTITY_TYPE_KEY.name(), cartId));
     SupervisorStrategy.restartWithBackoff(Duration.ofMillis(200), Duration.ofSeconds(5), 0.1);
     this.cartId = cartId;
@@ -54,19 +60,42 @@ public class ShoppingCart extends EventSourcedBehaviorWithEnforcedReplies<Shoppi
 
   @Override
   public CommandHandlerWithReply<ShoppingCartCommand, ShoppingCartEvent, ShoppingCartState> commandHandler() {
-    final CommandHandlerWithReplyBuilder<ShoppingCartCommand, ShoppingCartEvent, ShoppingCartState> builder = newCommandHandlerWithReplyBuilder();
-    builder.forAnyState().onCommand(AddItem.class, this::onAddItem);
-    return builder.build();
+    return openShoppingCart().orElse(checkedOutShoppingCart()).build();
   }
 
-  private ReplyEffect<ShoppingCartEvent, ShoppingCartState> onAddItem(ShoppingCartState state, AddItem command) {
+  private CommandHandlerWithReplyBuilderByState<ShoppingCartCommand, ShoppingCartEvent, ShoppingCartState, ShoppingCartState> openShoppingCart() {
+    final CommandHandlerWithReplyBuilder<ShoppingCartCommand, ShoppingCartEvent, ShoppingCartState> builder = newCommandHandlerWithReplyBuilder();
+    return builder.forState(state -> !state.isCheckedOut())
+        .onCommand(AddItem.class, this::onAddItem)
+        .onCommand(Checkout.class, this::onCheckout);
 
-    if(state.hasItem(command.itemId())){
+  }
+
+  private CommandHandlerWithReplyBuilderByState<ShoppingCartCommand, ShoppingCartEvent, ShoppingCartState, ShoppingCartState> checkedOutShoppingCart() {
+    final CommandHandlerWithReplyBuilder<ShoppingCartCommand, ShoppingCartEvent, ShoppingCartState> builder = newCommandHandlerWithReplyBuilder();
+    return builder.forState(ShoppingCartState::isCheckedOut)
+        .onCommand(AddItem.class, (state, command) -> replyError(command.replyTo(), "Can't add items to checked out cart"))
+        .onCommand(Checkout.class, (state, command) -> replyError(command.replyTo(), "Already checked out"));
+
+  }
+
+  private ReplyEffect<ShoppingCartEvent, ShoppingCartState> replyError(
+      ActorRef<StatusReply<Summary>> statusReplyActorRef,
+      String error) {
+    return Effect().reply(statusReplyActorRef, StatusReply.error(error));
+  }
+
+
+  private ReplyEffect<ShoppingCartEvent, ShoppingCartState> onAddItem(
+      ShoppingCartState state,
+      AddItem command) {
+
+    if (state.hasItem(command.itemId())) {
       return Effect().reply(command.replyTo(), StatusReply.error("Item " + command.itemId() + " ' was already added to this shopping cart."));
 
     }
 
-    if(command.quantity() <= 0){
+    if (command.quantity() <= 0) {
       return Effect().reply(command.replyTo(), StatusReply.error("Quantity must be > 0"));
     }
 
@@ -76,10 +105,36 @@ public class ShoppingCart extends EventSourcedBehaviorWithEnforcedReplies<Shoppi
 
   @Override
   public EventHandler<ShoppingCartState, ShoppingCartEvent> eventHandler() {
-    return newEventHandlerBuilder().forAnyState().onEvent(ItemAdded.class, this::updateItem).build();
+    return newEventHandlerBuilder().forAnyState()
+        .onEvent(ItemAdded.class, this::updateItem)
+        .onEvent(CheckedOut.class, this::handleCheckedOut).build();
   }
 
-  private ShoppingCartState updateItem(ShoppingCartState state, ItemAdded event) {
+  private ShoppingCartState updateItem(
+      ShoppingCartState state,
+      ItemAdded event) {
     return state.updateItem(event.itemId(), event.quantity());
   }
+
+
+  private ReplyEffect<ShoppingCartEvent, ShoppingCartState> onCheckout(
+      ShoppingCartState state,
+      Checkout command) {
+
+
+    if (state.isEmpty()) {
+      return Effect().reply(command.replyTo(), StatusReply.error("Cannot checkout an empty shopping cart."));
+    }
+
+    return Effect().persist(new CheckedOut(cartId, Instant.now()))
+        .thenReply(command.replyTo(), updatedCart -> StatusReply.success(updatedCart.toSummary()));
+  }
+
+
+  private ShoppingCartState handleCheckedOut(
+      ShoppingCartState state,
+      CheckedOut event) {
+    return state.checkout();
+  }
+
 }
