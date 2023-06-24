a4d9ff08394288c27e2eb688ad67215a9b516f7c
Vijay <vijay.k.vepakomma@gmail.com>
2023-06-24 17:48:54 -0400

    DDL Script take 2 (not ideal but kinda working)

--- MicroservicesTutorial/shopping-cart-service/kubernetes/deploy-with-kafka.sh
+++ MicroservicesTutorial/shopping-cart-service/kubernetes/deploy-with-kafka.sh
@@ -1,8 +1,10 @@
-kubectl create configmap create-tables --from-file=../ddl-scripts/create_tables.sql
-kubectl create configmap create-user-tables --from-file=../ddl-scripts/create_user_tables.sql
+
 kubectl apply -f namespace-k.yaml
 kubectl config set-context --current --namespace=shopping-cart-with-kafka
+kubectl create configmap create-tables --from-file=../ddl-scripts/create_tables.sql
+kubectl create configmap create-user-tables --from-file=../ddl-scripts/create_user_tables.sql
 kubectl apply -f zookeeper.yaml
 kubectl apply -f kafka-broker.yaml
 kubectl apply -f postgres.yaml
+kubectl apply -f init-db-scripts.yaml
 kubectl apply -f akka-cluster-kafka.yaml
